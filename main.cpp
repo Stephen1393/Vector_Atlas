15df9460bfde3f964c6c35d313211214fec36267
Stephen1393 <stephen.bending20@gmail.com>
2026-07-02 12:51:15 +0100

    lay out early architecture design/pipline

--- main.cpp
+++ main.cpp
@@ -2,23 +2,5 @@
  #include <string>
  #include <cctype>
 
-int main() {
-
-    std::string message = "Good afternoon. I hear we're having another heatwave.";
-    int words = 0;
-
-    for (int i{0}; i < message.size(); ++i) {
-        char letter = message[i];
-        
-        if (
-            (letter == ' ') ||
-            (i == message.size() - 1) 
-        )
-        {
-            words++; 
-        }
-    }
-
-    std:: cout << "words: " << words << '\n';
-}
+int main() {}
 
